2208991d6abc0b244fea01d1fa6afac71f252ec9
rukeni <rukeni@naver.com>
2022-09-11 10:53:37 +0900

    add env

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
     reactRemoveProperties: process.env.NODE_ENV !== 'test',
     removeConsole: process.env.NODE_ENV !== 'development',
   },
+  env: {
+    NEXT_PUBLIC_NOTION_API_TOKEN: process.env.NOTION_API_TOKEN,
+    NEXT_PUBLIC_NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
+  },
   eslint: { dirs: ['src'], ignoreDuringBuilds: true },
   poweredByHeader: false,
   reactStrictMode: true,
