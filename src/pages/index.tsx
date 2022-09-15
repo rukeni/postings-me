ccbe68521b403d13d3d40e95487de74edd29c7a8
rukeni <rukeni@naver.com>
2022-09-15 22:49:44 +0900

    reset Home Page component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
-import type { NextPage } from 'next';
-import Button from 'src/features/Button';
+import type { FC } from 'react';
 
-const Home: NextPage = () => {
-  return (
-    <h1 className='my-4 text-center text-4xl font-bold'>
-      Home
-      <Button label='Submit' />
-    </h1>
-  );
+type HomeProps = {
+  children: React.ReactNode;
+};
+
+const Home: FC<HomeProps> = () => {
+  return <div>Hello</div>;
 };
 
 export default Home;
